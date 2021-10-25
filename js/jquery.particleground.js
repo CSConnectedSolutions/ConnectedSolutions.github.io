c13f7973bc0668e06d0cb1eb5934390c74940399
Connected Solutions <92894077+CSConnectedSolutions@users.noreply.github.com>
2021-10-24 20:17:44 -0400

    particleground changes

--- js/jquery.particleground.js
+++ js/jquery.particleground.js
@@ -373,10 +373,10 @@
   };
 
   window[pluginName].defaults = {
-    minSpeedX: 0.1,
-    maxSpeedX: 0.7,
-    minSpeedY: 0.1,
-    maxSpeedY: 0.7,
+    minSpeedX: 0.05,
+    maxSpeedX: 0.3,
+    minSpeedY: 0.05,
+    maxSpeedY: 0.3,
     directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
     directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
     density: 10000, // How many particles will be generated: one particle every n pixels
@@ -387,7 +387,7 @@
     curvedLines: false,
     proximity: 100, // How close two dots need to be before they join
     parallax: true,
-    parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
+    parallaxMultiplier: 10, // The lower the number, the more extreme the parallax effect
     onInit: function() {},
     onDestroy: function() {}
   };
